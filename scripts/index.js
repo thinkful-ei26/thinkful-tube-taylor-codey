7ace0892a492b0a4e4509348cc141c11c4ed5868
Codey Worley <codeyworley@gmail.com>
2018-11-06 14:39:34 -0700

    changed to documented files

---
+++ scripts/index.js
@@ -0,0 +1,333 @@
+const API_KEY = 'AIzaSyCgxagrNYSSNWFSBWgSVAdLv5p8e8XEodE';
+
+const MOCK_DATA = {
+  "kind": "youtube#searchListResponse",
+  "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/agNYxBe38ZmeZDLAJbt3iyqfdS0\"",
+  "nextPageToken": "CAUQAA",
+  "regionCode": "US",
+  "pageInfo": {
+   "totalResults": 1000000,
+   "resultsPerPage": 5
+  },
+  "items": [
+   {
+    "kind": "youtube#searchResult",
+    "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/CIzHxn5Tg3A4s2nJ1YNkNe9bwj8\"",
+    "id": {
+     "kind": "youtube#video",
+     "videoId": "hY7m5jjJ9mM"
+    },
+    "snippet": {
+     "publishedAt": "2017-05-31T09:30:02.000Z",
+     "channelId": "UC9obdDRxQkmn_4YpcBMTYLw",
+     "title": "CATS will make you LAUGH YOUR HEAD OFF - Funny CAT compilation",
+     "description": "Cats are amazing creatures because they make us laugh all the time! Watching funny cats is the hardest try not to laugh challenge! Just look how all these cats ...",
+     "thumbnails": {
+      "default": {
+       "url": "https://i.ytimg.com/vi/hY7m5jjJ9mM/default.jpg",
+       "width": 120,
+       "height": 90
+      },
+      "medium": {
+       "url": "https://i.ytimg.com/vi/hY7m5jjJ9mM/mqdefault.jpg",
+       "width": 320,
+       "height": 180
+      },
+      "high": {
+       "url": "https://i.ytimg.com/vi/hY7m5jjJ9mM/hqdefault.jpg",
+       "width": 480,
+       "height": 360
+      }
+     },
+     "channelTitle": "Tiger FunnyWorks",
+     "liveBroadcastContent": "none"
+    }
+   },
+   {
+    "kind": "youtube#searchResult",
+    "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/Xv0f71hRivXbOFeZ14SKHWY1IJs\"",
+    "id": {
+     "kind": "youtube#video",
+     "videoId": "EtH9Yllzjcc"
+    },
+    "snippet": {
+     "publishedAt": "2017-11-18T21:00:00.000Z",
+     "channelId": "UC3rrzHpFzshYjIMk8YFc52w",
+     "title": "Try Not To Laugh Challenge - Funny Cat & Dog Vines compilation 2017",
+     "description": "Try Not To Laugh or Grin While Watching Funny Dogs & Cats Vines compilation 2017 IMPOSSIBLE CHALLENGE! Subscribe to CooL Vines ...",
+     "thumbnails": {
+      "default": {
+       "url": "https://i.ytimg.com/vi/EtH9Yllzjcc/default.jpg",
+       "width": 120,
+       "height": 90
+      },
+      "medium": {
+       "url": "https://i.ytimg.com/vi/EtH9Yllzjcc/mqdefault.jpg",
+       "width": 320,
+       "height": 180
+      },
+      "high": {
+       "url": "https://i.ytimg.com/vi/EtH9Yllzjcc/hqdefault.jpg",
+       "width": 480,
+       "height": 360
+      }
+     },
+     "channelTitle": "CooL Vines",
+     "liveBroadcastContent": "none"
+    }
+   },
+   {
+    "kind": "youtube#searchResult",
+    "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/VcTdH3FZoZafsgvhHDqKmj7GqKI\"",
+    "id": {
+     "kind": "youtube#video",
+     "videoId": "LJlYX7PZ9UU"
+    },
+    "snippet": {
+     "publishedAt": "2018-08-28T12:07:31.000Z",
+     "channelId": "UC5m164P4C-4F5bqs28HtI6w",
+     "title": "GIANT Maze Labyrinth for Cat Kittens. Can they EXIT?",
+     "description": "In this video I show you how to make Giant Cardboard Maze Game for your cat or kittens. It's a fun project for you and your pets. If you like the video, share it, like ...",
+     "thumbnails": {
+      "default": {
+       "url": "https://i.ytimg.com/vi/LJlYX7PZ9UU/default.jpg",
+       "width": 120,
+       "height": 90
+      },
+      "medium": {
+       "url": "https://i.ytimg.com/vi/LJlYX7PZ9UU/mqdefault.jpg",
+       "width": 320,
+       "height": 180
+      },
+      "high": {
+       "url": "https://i.ytimg.com/vi/LJlYX7PZ9UU/hqdefault.jpg",
+       "width": 480,
+       "height": 360
+      }
+     },
+     "channelTitle": "EverXFun",
+     "liveBroadcastContent": "none"
+    }
+   },
+   {
+    "kind": "youtube#searchResult",
+    "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/rDd-IbPkFNfqp9EIZmHIGGpldyw\"",
+    "id": {
+     "kind": "youtube#video",
+     "videoId": "kO4wYbkE408"
+    },
+    "snippet": {
+     "publishedAt": "2018-07-29T17:32:26.000Z",
+     "channelId": "UCeL2LSl91k2VccR7XEh5IKg",
+     "title": "LIVE: Hyatt the blind feral cat - TinyKittens.com",
+     "description": "This is Hyatt's story so far: https://youtu.be/kbzQj5CtuiQ Hyatt's roommate is Salia, who is from the same feral colony and is being treated for an ear polyp and ...",
+     "thumbnails": {
+      "default": {
+       "url": "https://i.ytimg.com/vi/kO4wYbkE408/default_live.jpg",
+       "width": 120,
+       "height": 90
+      },
+      "medium": {
+       "url": "https://i.ytimg.com/vi/kO4wYbkE408/mqdefault_live.jpg",
+       "width": 320,
+       "height": 180
+      },
+      "high": {
+       "url": "https://i.ytimg.com/vi/kO4wYbkE408/hqdefault_live.jpg",
+       "width": 480,
+       "height": 360
+      }
+     },
+     "channelTitle": "TinyKittens HQ",
+     "liveBroadcastContent": "live"
+    }
+   },
+   {
+    "kind": "youtube#searchResult",
+    "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/w3Yt8Ap-2nftMsG8ImFVsH_ZZwE\"",
+    "id": {
+     "kind": "youtube#video",
+     "videoId": "oZFAcp-Qfbs"
+    },
+    "snippet": {
+     "publishedAt": "2018-10-24T14:24:19.000Z",
+     "channelId": "UCNo5PGwGmfnEprEUUb9AGgw",
+     "title": "World's Most Obedient Cat",
+     "description": "Prince Michael takes on a few new responsibilities while Aaron's caught under the weather. T-Shirt from video available here: https://shop.aaronsanimals.com/ ...",
+     "thumbnails": {
+      "default": {
+       "url": "https://i.ytimg.com/vi/oZFAcp-Qfbs/default.jpg",
+       "width": 120,
+       "height": 90
+      },
+      "medium": {
+       "url": "https://i.ytimg.com/vi/oZFAcp-Qfbs/mqdefault.jpg",
+       "width": 320,
+       "height": 180
+      },
+      "high": {
+       "url": "https://i.ytimg.com/vi/oZFAcp-Qfbs/hqdefault.jpg",
+       "width": 480,
+       "height": 360
+      }
+     },
+     "channelTitle": "AaronsAnimals",
+     "liveBroadcastContent": "none"
+    }
+   }
+  ]
+ };
+
+/*
+  We want our store to hold an array of "decorated" video objects - i.e. objects that
+  have been transformed into ONLY the necessary data we're displaying on our page. 
+  (Removes extraneous dataset from Youtube.)
+  
+  Example decorated video object:
+  
+  {
+    id: '98ds8fbsdy67',
+    title: 'Cats dancing the Macarena',
+    thumbnail: 'https://img.youtube.com/some/thumbnail.jpg'
+  }
+*/
+const store = {
+  videos: []
+};
+
+// TASK: Add the Youtube Search API Base URL here:
+// Documentation is here: https://developers.google.com/youtube/v3/docs/search/list#usage
+const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+/**
+ * @function fetchVideos
+ * Async function, responsible for calling the Youtube API with jQuery, constructing
+ * the correct query object, and passing along the callback into the AJAX call.
+ * @param {string}   searchTerm
+ * @param {function} callback
+ */
+// TASK:
+// 1. Use `searchTerm` to construct the right query object based on the Youtube API docs
+//    - Refer to curriculum assignment for help with the required parameters
+// 2. Make a getJSON call using the query object and sending the provided callback in 
+//    as the last argument
+//
+// TEST IT! Execute this function and console log the results inside the callback.
+const fetchVideos = function(searchTerm, callback) {
+  const query = {
+    q: searchTerm,
+    key: API_KEY,
+    part: 'snippet',
+  }
+  $.getJSON(BASE_URL, query, (response) => callback(response));
+};
+
+/**
+ * @function decorateResponse
+ * Uses Youtube API response to create an array of "decorated" video objects as 
+ * defined at the top of the file.
+ * @param   {object} response - should match Youtube API response shape
+ * @returns {array}
+ */
+// TASK:
+// 1. Map through the response object's `items` array
+// 2. Return an array of objects, where each object contains the keys `id`, `title`, 
+//    `thumbnail` which each hold the appropriate values from the API item object. You 
+//    WILL have to dig into several nested properties!
+//
+// TEST IT! Grab an example API response and send it into the function - make sure
+// you get back the object you want.
+const decorateResponse = function(response) {
+  console.log(response.items[0])
+  return response.items.map(item =>{ 
+    return { 
+      id: item.id.videoId, 
+      title: item.snippet.title, 
+      thumbnail: item.snippet.thumbnails.default.url, 
+    }; 
+  });
+}
+
+const value = fetchVideos('cats', decorateResponse);
+console.log(value)
+
+/**
+ * @function generateVideoItemHtml
+ * Template function, creates an HTML string from a single decorated video object
+ * @param   {object} video - decorated video object
+ * @returns {string} HTML 
+ */
+// TASK:
+// 1. Using the decorated object, return an HTML string containing all the expected
+// TEST IT!
+const generateVideoItemHtml = function(video) {
+  return `
+    <li data_video_ID="${video.id}">
+    <h3>${video.title}</h3>
+    <img src="${video.thumbnail}">
+    </li>
+  `
+};
+
+/**
+ * @function addVideosToStore
+ * Store modification function to set decorated video objects
+ * @param {array} videos - decorated video objects
+ */
+// TASK:
+// 1. Set the received array as the value held in store.videos
+// TEST IT!
+const addVideosToStore = function(videos) {
+  store.videos = videos;
+};
+
+/**
+ * @function render
+ * Responsible for scanning store and rendering the video list to DOM
+ */
+// TASK:
+// 1. Map through `store.videos`, sending each `video` through `generateVideoItemHtml`
+// 2. Add this array of DOM elements to the appropriate DOM element
+// TEST IT!
+const render = function() {
+  const html = store.videos.map( video => 
+    generateVideoItemHtml(video)
+  )
+  .join('');
+  $('.results').html(html);
+};
+
+/**
+ * @function handleFormSubmit
+ * Adds form "submit" event listener that 1) initiates API call, 2) modifies store,
+ * and 3) invokes render
+ */
+// TASK:
+// 2. Add an event listener to the form that will:
+//   a) Prevent default event
+//   b) Retrieve the search input from the DOM
+//   c) Clear the search input field
+//   d) Invoke the `fetchVideos` function, sending in the search value
+//   e) Inside the callback, send the API response through the `decorateResponse` function
+//   f) Inside the callback, add the decorated response into your store using the 
+//      `addVideosToStore` function
+//   g) Inside the callback, run the `render` function 
+// TEST IT!
+
+const handleFormSubmit = function(event) {
+  
+  $('form').submit('#search-term', event => {
+    event.preventDefault();
+
+    const val = $('#search-term').val();
+    $('#search-term').val('');
+
+  })
+};
+
+//console.log('step 11 - all functions and varaibles declared in previous steps')
+// When DOM is ready:
+$(function () {
+  //console.log('step 12 - run handleformsubmit')
+  handleFormSubmit();
+});
